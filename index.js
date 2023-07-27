189e3a9449ef323bc43a32c8a23055c6cdf8be57
saitokojiro <kazutonight@gmail.comm>
2023-07-27 23:43:44 +0200

    update

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
 let playersJson = [];
-let getTypePlayer = (value) => {
-    let PlayerName = document.querySelector(".inputPlayerName").value;
-    let noSpacing = PlayerName.split(" ").join("");
-
-    if (noSpacing !== "") {
-        if (playersJson.length == 0) {
-            playersJson.push({
-                name: PlayerName,
-                playerSelected: value,
-            });
-
-            let settingPlayer = document.querySelector(".player" + value);
-
-            settingPlayer.innerHTML = " Player " + 1 + ": " + PlayerName;
-            let btnpO = document.querySelector(".s_player_one");
-            let btnpT = document.querySelector(".s_player_two");
+let getTypePlayer = (value, e) => {
+    let btnpO = document.querySelector(".s_player_one");
+    let btnpT = document.querySelector(".s_player_two");
             btnpO.disabled = true
             btnpT.disabled = true
-        } else {
-
-
-            playersJson.forEach((el) => {
-                //need verification with server
-                
-                console.log(playersJson.some(e => e.Name !== value))
-                if (playersJson.some(e => e.playerSelected !== value)) {
-                    
-                        playersJson.push({
-                            name: PlayerName,
-                            playerSelected: value,
-                        });
-
-                        let settingPlayer = document.querySelector(".player" + value);
-
-                        settingPlayer.innerHTML = " Player " + 1 + ": " + PlayerName;
-                    
-                } else {
-                     alert("cannot select player "+value);
-                    console.log(playersJson);
-                }
-            });
-        }
-    } else {
-        alert("pseudo invalid");
-    }
+    
+            let valueAdv =  value == 1 ? 2 : 1 
+            let setPlayer = document.querySelector(".player"+value);
+            let setPlayer2 = document.querySelector(".player"+valueAdv);
+            setPlayer.innerHTML += " (you)"
+            setPlayer.classList.add("you")
+            setPlayer2.innerHTML += " (adv)"
+            setPlayer2.classList.add("adv")
+            
 };
-let SelectCaseType = () => { };
+let SelectCaseType = (e) => {
+    
+    let youPlayer = document.querySelector(".you")
+    
+    if(youPlayer !== null) {
+    if(youPlayer.classList[0] == "player1"){
+        if(e.target.innerHTML === "") {
+            e.target.innerHTML = "X"
+            e.target.classList.add("YouAdd")
+            console.log(e.target)
+        }
+    }else{
+        if(e.target.innerHTML === "") {
+            e.target.innerHTML = "O"
+            e.target.classList.add("YouAdd")
+            console.log(e.target)
+        }
+    }
+    }else{
+        alert("you have not select")
+    }
+    
+ };
+
+
+ let checking =() => {
+    
+ }
